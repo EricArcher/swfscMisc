43159a17ab2120e139414e4f0bf947d5f9c92c81
Eric Archer <eric.archer@noaa.gov>
2016-02-03 16:30:43 -0800

    made transparent vectorized

--- swfscMisc/R/transparent.R
+++ swfscMisc/R/transparent.R
@@ -10,16 +10,20 @@
 #' @examples
 #' 
 #' pct <- seq(0, 100, by = 10)
-#' plot(pct, pct, bg = transparent("red", pct), pch = 21, cex = 3, xlab = "X", ylab = "Y")
+#' plot(pct, pct, bg = transparent("red", pct), pch = 21, cex = 4, xlab = "X", ylab = "Y")
 #' 
 #' @export
 #' 
 transparent <- function(col, percent = 50) {
-  rgb.val <- col2rgb(col)
-  col.names <- paste(col, percent, sep = ".")
-  rgb(
-    red = rgb.val[1], green = rgb.val[2], blue = rgb.val[3], 
-    alpha = (100 - percent) * 255 / 100, names = col.names, maxColorValue = 255
-  )
+  percent <- rep(percent, length.out = length(col))
+  trans.cols <- sapply(1:length(col), function(i) {
+    rgb.val <- col2rgb(col[i])
+    rgb(
+      red = rgb.val[1], green = rgb.val[2], blue = rgb.val[3], 
+      alpha = (100 - percent[i]) * 255 / 100, maxColorValue = 255
+    )
+  })
+  names(trans.cols) <- paste(col, percent, sep = ".")
+  return(trans.cols)
 }
   
